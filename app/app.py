cdf6d7f57ff01beaa8b65b88b27491a2005ca769
evelyneesther <122478608+evelyneesther@users.noreply.github.com>
2023-03-26 17:16:37 +0000

    added test dataset, added displaying dataset

--- app/app.py
+++ app/app.py
@@ -1,48 +1,19 @@
-import os
+from _collections_abc import Container
+from flask import Flask
+import json
 
-from flask import Flask, render_template
-from . import settings, controllers, models
-from .extensions import db
+app = Flask(__name__)
 
-project_dir = os.path.dirname(os.path.abspath(__file__))
+@app.route("/")
+def hello_world():
+    f = open('nobel prizes.json')   #Dataset from https://github.com/jdorfman/awesome-json-datasets#nobel-prize
+    prizes=json.load(f)
+    response="<p>"
+    for key in prizes["prizes"]:
+        for thing in key:
+            response+=str(prizes["prizes"][prizes["prizes"].index(key)][thing])+"<br>"
+        
+        response+="<br>"
+    response+="</p>"
 
-def create_app(config_object=settings):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(config_object)
-
-    register_extensions(app)
-    register_blueprints(app)
-    register_errorhandlers(app)
-    return app
-
-def register_extensions(app):
-    """Register Flask extensions."""
-    db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
-    return None
-
-def register_blueprints(app):
-    """Register Flask blueprints."""
-    app.register_blueprint(controllers.home.blueprint)
-    app.register_blueprint(controllers.auth.blueprint)
-    app.register_blueprint(controllers.tutorial.blueprint)
-    return None
-
-def register_errorhandlers(app):
-    """Register error handlers."""
-    @app.errorhandler(401)
-    def internal_error(error):
-        return render_template('401.html'), 401
-
-    @app.errorhandler(404)
-    def page_not_found(error):
-        return render_template('404.html'), 404
-
-    @app.errorhandler(500)
-    def internal_error(error):
-        return render_template('500.html'), 500
-
-    return None
+    return response
